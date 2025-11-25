2f93207c75ca3b7475660890e420cee9f0267516
minseong1 <104720807+alstjd2627@users.noreply.github.com>
2025-11-25 20:26:48 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 247ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 251125/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word1 = sc.next();
+        String word2 = sc.next();
+        char[] arr1 = word1.toCharArray();
+        char[] arr2 = word2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        if(Arrays.equals(arr1,arr2)){
+            System.out.println("Yes");
+        } else{
+            System.out.println("No");
+        }
+
+    }
+}
