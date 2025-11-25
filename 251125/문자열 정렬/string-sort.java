4a4b9a003dff20258ac568b80fd43b9d8581b73d
minseong1 <104720807+alstjd2627@users.noreply.github.com>
2025-11-25 19:47:31 +0900

    Title: 문자열 정렬, Time: 116ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251125/문자열 정렬/string-sort.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+       char[] chars = s.toCharArray();
+       Arrays.sort(chars);
+       String sortedStr = new String(chars);
+       System.out.println(sortedStr);
+    }
+}
