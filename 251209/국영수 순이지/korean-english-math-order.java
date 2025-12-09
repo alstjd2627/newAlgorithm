9d2d3215a4ea9f6204ceb83ea0142e7e436f7413
minseong1 <104720807+alstjd2627@users.noreply.github.com>
2025-12-09 20:48:42 +0900

    Title: 국영수 순이지, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251209/국영수 순이지/korean-english-math-order.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+class Student implements Comparable<Student>{
+    String name;
+    int kor, eng, math;
+    
+    public Student(String name, int kor, int eng, int math){
+        this.name = name;
+        this.kor = kor;
+        this. eng = eng;
+        this. math = math;
+    }
+    
+    @Override
+    public int compareTo(Student student){
+        if(this.kor == student.kor){
+            if(this.eng == student.eng){
+                return student.math - this.math;
+            }
+            return student.eng - this.eng;
+        }
+        return student.kor - this.kor;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String name;
+        int kor,math,eng;
+        Student[] students = new Student[n];
+        for (int i = 0; i < n; i++) {
+            name = sc.next();
+            kor = sc.nextInt();
+            eng = sc.nextInt();
+            math = sc.nextInt();
+            students[i] = new Student(name,kor,eng,math);
+        }
+        
+        Arrays.sort(students);
+        for (int i = 0; i < n; i++) {
+            System.out.println(students[i].name + " " + students[i].kor + " " + students[i].eng + " " + students[i].math);
+        }
+    }
+}
