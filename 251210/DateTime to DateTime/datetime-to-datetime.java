49c395b853de1c47dd473ad2ed026b11d2bace11
minseong1 <104720807+alstjd2627@users.noreply.github.com>
2025-12-10 21:54:33 +0900

    Title: DateTime to DateTime, Time: 136ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251210/DateTime to DateTime/datetime-to-datetime.java
+++ 251210/DateTime to DateTime/datetime-to-datetime.java
@@ -5,13 +5,11 @@ public class Main {
         int A = sc.nextInt();
         int B = sc.nextInt();
         int C = sc.nextInt();
-        int total1 = 11 + 11*60 + 10 * 24 * 60;
-        int total2 = 0;
-        if(A !=0){
-             total2 = C + B * 60 + (A-1) * 24 * 60;
-        } else{
-             total2 = C + B * 60;
-        }
+        long total1 = 11 + 11*60 + 10 * 24 * 60;
+        long total2 = 0;
+        
+        total2 = C + B * 60 + (A-1) * 24 * 60;
+       
 
         System.out.println(total2-total1);
     }
