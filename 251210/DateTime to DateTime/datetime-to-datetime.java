0fa315a10ed69ff101b97ff9ea456044389d3f41
minseong1 <104720807+alstjd2627@users.noreply.github.com>
2025-12-10 21:52:05 +0900

    Title: DateTime to DateTime, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 251210/DateTime to DateTime/datetime-to-datetime.java
+++ 251210/DateTime to DateTime/datetime-to-datetime.java
@@ -6,7 +6,12 @@ public class Main {
         int B = sc.nextInt();
         int C = sc.nextInt();
         int total1 = 11 + 11*60 + 10 * 24 * 60;
-        int total2 = C + B * 60 + (A-1) * 24 * 60;
+        if(A !=0){
+            int total2 = C + B * 60 + (A-1) * 24 * 60;
+        } else{
+            int total2 = C + B * 60;
+        }
+
         System.out.println(total2-total1);
     }
 }
