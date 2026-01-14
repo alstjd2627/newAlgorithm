9ef91fdb1ddb0232c5faca286b0382f85504a7cd
minseong1 <104720807+alstjd2627@users.noreply.github.com>
2026-01-14 22:32:35 +0900

    Title: 흰검 칠하기, Time: 215ms, Memory: 19MB, Status: Passed - Codetree

--- 260114/흰검 칠하기/painting-white-black.java
+++ 260114/흰검 칠하기/painting-white-black.java
@@ -1,50 +1,53 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] white = new int[200002];
-        int[] black = new int[200002];
-        int[] last = new int[200002];
-        int current = 100000;
+
+        int SIZE = 400000;        
+        int OFFSET = 200000;        
+
+        int[] white = new int[SIZE];
+        int[] black = new int[SIZE];
+        boolean[] gray = new boolean[SIZE];
+        int[] last = new int[SIZE];
+
+        int cur = OFFSET;
+
         for (int i = 0; i < N; i++) {
             int x = sc.nextInt();
             char d = sc.next().charAt(0);
-            for(int j = 0 ; j < x; j++){
-                if(d == 'R'){
-                    if(black[current] < 2){
-                        black[current]++;
-                        last[current] = 2;
+
+            for (int j = 0; j < x; j++) {
+                if (!gray[cur]) {
+                    if (d == 'R') {
+                        if (black[cur] < 2) black[cur]++;
+                        if (white[cur] < 2 || black[cur] < 2) last[cur] = 2; 
+                    } else {
+                        if (white[cur] < 2) white[cur]++;
+                        if (white[cur] < 2 || black[cur] < 2) last[cur] = 1;
                     }
-                    if(black[current] >= 2 && white[current] >= 2){
-                        last[current] = 3;
+
+                    if (white[cur] >= 2 && black[cur] >= 2) {
+                        gray[cur] = true;
+                        last[cur] = 3;
                     }
-                    current++;
-                } else{
-                    if(white[current] < 2){
-                        white[current]++;
-                        last[current] = 1;
-                    }
-                    if(black[current] >= 2 && white[current] >= 2){
-                        last[current] = 3;
-                    }
-                    current--;
+                }
+
+                if (j < x - 1) { 
+                    if (d == 'R') cur++;
+                    else cur--;
                 }
             }
-            if(d =='R') current--;
-            else current++;
-            
         }
-        int whiteCount = 0;
-        int blackCount = 0;
-        int grayCount = 0;
-        for(int i = 0; i < last.length; i++){
-            if(last[i] == 1) whiteCount++;
-            else if(last[i]==2) blackCount++;
-            else if(last[i] == 3) grayCount++;
-        }
-        System.out.println(whiteCount + " " + blackCount + " " + grayCount);
 
-        
+        int w = 0, b = 0, g = 0;
+        for (int i = 0; i < SIZE; i++) {
+            if (last[i] == 1) w++;
+            else if (last[i] == 2) b++;
+            else if (last[i] == 3) g++;
+        }
+        System.out.println(w + " " + b + " " + g);
     }
 }
