d61fb58bfe0d308e5b2c2f042207f613007b6eef
minseong1 <104720807+alstjd2627@users.noreply.github.com>
2026-01-15 13:18:35 +0900

    Title: 흰검 칠하기, Time: 185ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 260115/흰검 칠하기/painting-white-black.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] white = new int[400002];
+        int[] black = new int[400002];
+        int[] last = new int[400002];
+        int current = 200000;
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            char d = sc.next().charAt(0);
+            for(int j = 0 ; j < x; j++){
+                if(d == 'R'){
+                    if(black[current] < 2){
+                        black[current]++;
+                        last[current] = 2;
+                    }
+                    if(black[current] >= 2 && white[current] >= 2){
+                        last[current] = 3;
+                    }
+                    current++;
+                } else{
+                    if(white[current] < 2){
+                        white[current]++;
+                        last[current] = 1;
+                    }
+                    if(black[current] >= 2 && white[current] >= 2){
+                        last[current] = 3;
+                    }
+                    current--;
+                }
+            }
+            if(d =='R') current--;
+            else current++;
+            
+        }
+        int whiteCount = 0;
+        int blackCount = 0;
+        int grayCount = 0;
+        for(int i = 0; i < last.length; i++){
+            if(last[i] == 1) whiteCount++;
+            else if(last[i]==2) blackCount++;
+            else if(last[i] == 3) grayCount++;
+        }
+        System.out.println(whiteCount + " " + blackCount + " " + grayCount);
+
+        
+    }
+}
