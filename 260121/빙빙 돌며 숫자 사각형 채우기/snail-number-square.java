15664cf872db1a3f742378ccb1174709ad5c848c
minseong1 <104720807+alstjd2627@users.noreply.github.com>
2026-01-21 17:19:13 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260121/빙빙 돌며 숫자 사각형 채우기/snail-number-square.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    static int[] dc = {1,0,-1,0};
+    static int[] dr = {0,1,0,-1};
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] map = new int[N][M];
+        int cc = 0;
+        int cr = 0;
+        int map[cc][cr] =1;
+        int count = 1;
+        int nc = 0; int nr = 0;
+        int dir = 0;
+        for(int i = 1 ; i < N*M; i++){
+            count++;
+            nc = cc + dc[dir];
+            nr = cr + dr[dir];
+            if(nc < 0 || nr < 0|| nc >= M || nr >= N || map[nr][nc] != 0){
+                dir = (dir+1) % 4;
+                nc = cc + dc[dir];
+                nr = cr + dr[dir];
+            }
+            cc =nc;
+            cr =nr;
+            map[cr][cc] = count;
+        }
+        for(int i = 0 ; i < N; i++){
+            for(int j = 0 ; j < M; j++){
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
