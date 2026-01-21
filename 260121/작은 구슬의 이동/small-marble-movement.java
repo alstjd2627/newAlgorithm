a76b944aab0e9a440a35070924e671cc5ec0c0a2
minseong1 <104720807+alstjd2627@users.noreply.github.com>
2026-01-21 17:01:48 +0900

    Title: 작은 구슬의 이동, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 260121/작은 구슬의 이동/small-marble-movement.java
+++ 260121/작은 구슬의 이동/small-marble-movement.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     static int[] dc = {1,0,0,-1};
-    static int[] dr = {0,1,-1,0};
+    static int[] dr = {0,-1,1,0};
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
